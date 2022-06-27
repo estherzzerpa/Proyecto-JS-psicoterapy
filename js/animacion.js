dd04c923fe428b8ab0474ee968aa01e25c792071
estherzzerpa <esterzerpa201@gmail.com>
2022-06-27 00:26:20 -0300

    primer commir

---
+++ js/animacion.js
@@ -0,0 +1,17 @@
+
+
+// ANIMACION
+
+export let section = document.querySelector(".animacion");
+export let text = document.querySelector(".text")
+export let innerText = document.querySelector(".innerText")
+
+ window.addEventListener("scroll", function(){
+
+    let value = window.scrollY;
+    section.style.clipPath = `circle(${value}px at center center)`;
+    text.style.left = 100 - value / 5 + "%"
+    innerText.style.left = 100 - value / 5 + "%"
+
+});
+
