d2ed461417b790fb401237206838b5add117f2eb
estherzzerpa <esterzerpa201@gmail.com>
2022-08-02 21:28:55 -0300

    mobile

--- src/animacion.js
+++ src/animacion.js
@@ -14,3 +14,22 @@ let innerText = document.querySelector(".innerText")
     innerText.style.left = 100 - value / 5 + "%"
 });
 
+ // menu responsive 
+document.querySelector(".bars__efect").addEventListener("click", activeMenu)
+const menuDesplegable = document.querySelector(".container__opciones")
+
+const line1__bars = document.querySelector(".line1__bars-menu")
+const line2__bars = document.querySelector(".line2__bars-menu")
+const line3__bars = document.querySelector(".line3__bars-menu")
+
+function activeMenu(){
+    line1__bars.classList.toggle("activeline1__bars-menu")
+    line2__bars.classList.toggle("activeline2__bars-menu")
+    line3__bars.classList.toggle("activeline3__bars-menu")
+    menuDesplegable.classList.toggle("menu_active")
+
+    console.log("funciona")
+
+}
+
+
